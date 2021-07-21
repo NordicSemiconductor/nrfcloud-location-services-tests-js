2c1146eddee6b03c16e2c7c8e75b4a9382b8b38c
Markus Tacker <m@coderbyheart.com>
2021-07-21 19:13:57 +0200

    test(agps): ensure range header is honored

--- agps.spec.js
+++ agps.spec.js
@@ -23,4 +23,23 @@ describe("AGPS", () => {
     });
     expect(res.length).toBe(chunkSize);
   });
+  it("should chunk large responses", async () => {
+    const res = await get(
+      "location/agps",
+      {
+        deviceIdentifier: "TestClient",
+        mcc: 242,
+        mnc: 2,
+        eci: 33703712,
+        tac: 2305,
+        requestType: "custom",
+        customTypes: 2,
+      },
+      {
+        "Content-Type": "application/octet-stream",
+        Range: `bytes=0-2000`,
+      }
+    );
+    expect(res.length).toBeLessThan(2000);
+  });
 });
