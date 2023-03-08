4cb15f1769880a34670f7f6e6af264e4e7c8b15b
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-03-08 15:28:34 +0100

    test(ground-fix): add sample for combined location data

--- api-verification/ground-fix.spec.ts
+++ api-verification/ground-fix.spec.ts
@@ -1,4 +1,12 @@
-import * as path from 'path'
+import combinedLteWifi from '../test-data/combined-lte-wifi.json'
+import mcellpayload1 from '../test-data/mcellpayload1.json'
+import mcellpayload2 from '../test-data/mcellpayload2.json'
+import mcellpayload3 from '../test-data/mcellpayload3.json'
+import mcellpayload4 from '../test-data/mcellpayload4.json'
+import mcellpayload5 from '../test-data/mcellpayload5.json'
+import scellpayload1 from '../test-data/scellpayload1.json'
+import wifipayload1 from '../test-data/wifipayload1.json'
+import wifipayload2 from '../test-data/wifipayload2.json'
 import { apiClient, tokenAuthorization } from './api-client'
 
 const { post } = apiClient({
@@ -27,14 +35,6 @@ declare global {
 			toMatchLocation: (expected: Location) => CustomMatcherResult
 		}
 	}
-}
-
-/**
- * Used to dynamically load a JSON file.
- */
-const load = async (filename: string) => {
-	const mod = await import(filename)
-	return mod.default
 }
 
 expect.extend({
@@ -84,12 +84,9 @@ expect.extend({
 describe('ground fix services', () => {
 	describe('multi-cell location', () => {
 		it.each([
+			[mcellpayload1, { lat: 63.418807, lon: 10.412916, uncertainty: 2238 }],
 			[
-				'mcellpayload1.json',
-				{ lat: 63.418807, lon: 10.412916, uncertainty: 2238 },
-			],
-			[
-				'mcellpayload2.json',
+				mcellpayload2,
 				{
 					uncertainty: 2139,
 					lat: 63.42811704,
@@ -97,7 +94,7 @@ describe('ground fix services', () => {
 				},
 			],
 			[
-				'mcellpayload3.json',
+				mcellpayload3,
 				{
 					uncertainty: 2139,
 					lat: 63.42811704,
@@ -105,63 +102,60 @@ describe('ground fix services', () => {
 				},
 			],
 			[
-				'mcellpayload4.json',
+				mcellpayload4,
 				{
 					uncertainty: 440,
 					lat: 63.42557256,
 					lon: 10.43830085,
 				},
 			],
-		])('should resolve %s to %j', async (file, expectedLocation) => {
-			const filename = path.join(process.cwd(), 'test-data', file)
-			const payload = await load(filename)
+			[
+				combinedLteWifi,
+				{
+					uncertainty: 440,
+					lat: 63.42557256,
+					lon: 10.43830085,
+				},
+			],
+		])('should resolve %s to %j', async (payload, expectedLocation) => {
 			expect(
 				await post({ resource: 'location/ground-fix', payload }),
 			).toMatchLocation(expectedLocation)
 		})
 
-		it('should resolve this multi-cell result', async () => {
-			const filename = path.join(
-				process.cwd(),
-				'test-data',
-				'mcellpayload5.json',
-			)
-			const payload = await load(filename)
-			await expect(
+		it('should resolve this multi-cell result', async () =>
+			expect(
 				post({
 					resource: 'location/ground-fix',
-					payload,
+					payload: mcellpayload5,
 				}),
-			).resolves.not.toBeUndefined()
-		})
+			).resolves.not.toBeUndefined())
 	})
 
 	describe('single-cell location', () => {
 		it.each([
 			[
-				'scellpayload1.json',
+				scellpayload1,
 				{
 					uncertainty: 2416,
 					lat: 63.42373967,
 					lon: 10.38332462,
 				},
 			],
-		])('should resolve %j to %j', async (file, expectedLocation) => {
-			const filename = path.join(process.cwd(), 'test-data', file)
-			const payload = await load(filename)
+		])('should resolve %j to %j', async (payload, expectedLocation) =>
 			expect(
 				await post({
 					resource: 'location/ground-fix',
 					payload,
 				}),
-			).toMatchLocation(expectedLocation)
-		})
+			).toMatchLocation(expectedLocation),
+		)
 	})
 
 	describe('wifi location', () => {
 		it.each([
 			[
-				'wifipayload1.json',
+				wifipayload1,
 				{
 					lat: 63.4214305,
 					lon: 10.437707,
@@ -169,23 +163,21 @@ describe('ground fix services', () => {
 				},
 			],
 			[
-				'wifipayload2.json',
+				wifipayload2,
 				{
 					lat: 63.4213862,
 					lon: 10.4375898,
 					uncertainty: 14.772,
 				},
 			],
-		])('should resolve %s to %j', async (file, expectedLocation) => {
-			const filename = path.join(process.cwd(), 'test-data', file)
-			const payload = await load(filename)
+		])('should resolve %s to %j', async (payload, expectedLocation) => {
 			expect(
 				await post({ resource: 'location/ground-fix', payload }),
 			).toMatchLocation(expectedLocation)
 		})
 
-		it('should require at least 2 access points with MAC addresses', async () => {
-			await expect(
+		it('should require at least 2 access points with MAC addresses', async () =>
+			expect(
 				post({
 					resource: 'location/ground-fix',
 					payload: {
@@ -198,7 +190,6 @@ describe('ground fix services', () => {
 						},
 					},
 				}),
-			).rejects.toThrow('Request failed: 422')
-		})
+			).rejects.toThrow('Request failed: 422'))
 	})
 })
